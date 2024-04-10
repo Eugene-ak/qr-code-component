e24de7974a3e23050357a7841d1e54e018854070
Eugene-ak <amedioreugene@gmail.com>
2024-04-10 19:01:14 +0000

    Complete project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import "./index.scss";
+import "./styles/index.scss";
 import qr from "/images/image-qr-code.png";
 
 function App() {
   return (
     <>
-      <div>
+      <div className="card">
         <img src={qr} alt="QR" />
-        <h4>Improve your front-end skills by building projects</h4>
-        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        <div className="description">
+          <h4>Improve your front-end skills by building projects</h4>
+          <p>
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
